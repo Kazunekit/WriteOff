7c63993bb4b659a59ed8fa339dccf62e0bbaf49b
Jaroslav Bisikirski <kazunekit@gmail.com>
2013-06-29 16:26:46 +0200

    No idea

--- lib/WriteOff/Schema/ResultSet/Event.pm
+++ lib/WriteOff/Schema/ResultSet/Event.pm
@@ -35,22 +35,4 @@ sub finished {
 	);
 }
 
-sub sanitized_search {
-    my ( $self, $search ) = @_;
-    return $self->search($search,
-        {
-            'select'   =>  ['id','prompt',\'IFNULL(blurb,"None")','wc_min','wc_max',
-                            \'CASE rule_set WHEN 1 THEN "T" WHEN 2 THEN "M" ELSE "E" END', \'IFNULL(custom_rules,"None")',
-                            'start','prompt_voting','prompt_voting',\'IFNULL(art,fic)',
-                            \'IFNULL(art,0)',\'IFNULL(art_end,0)','fic','fic_end',\'IFNULL(prelim,0)',
-                            'public',\'IFNULL(private,0)', 'end', 'created' ],
-            'as'        =>  ['Event.ID', 'Info.Initial Prompt', 'Info.Blurb', 'Word Count.Min', 'Word Count.Max', 
-                            'Rules.Content Level', 'Rules.Custom',
-                            'Prompt Submission.Start','Prompt Submission.End', 'Prompt Voting.Start', 'Prompt Voting.End',
-                            'Art.Start', 'Art.End', 'Fic.Start', 'Fic.End', 
-                            'Voting.Preliminaries Start','Voting.Main Start', 'Voting.Finals Start', 'Voting.Ends', 'Event.Created at']
-        },
-    );
-}
-
 1;
